ef91284d0033b60a3f26f63e07161e9ebde91ac2
pburb5029 <pburb5029@365.elmhurst.edu>
2025-09-06 21:01:15 -0500

    added prototype1

---
+++ CGE401Prototype1/Assets/Scripts/LoseOnFall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class LoseOnFall : MonoBehaviour
+{
+    //public Text textbox;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1)
+        {
+            //textbox.text = "You Lose!";
+            ScoreManager.gameOver = true;
+        }
+        
+    }
+}
---
+++ CGE401Prototype1/Assets/Scripts/PlayeEnterTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class PlayeEnterTrigger : MonoBehaviour
+{
+    //set this reference in the inspector
+    //public Text textbox;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerZone"))
+        {
+            //textbox.text = "You Win!";
+            ScoreManager.score++;
+        }
+    }
+}
