0921b2628c0788935e254b159e8da4b07a3ca6b0
pburb5029 <pburb5029@365.elmhurst.edu>
2025-09-28 14:06:57 -0500

    finished challenge3

---
+++ CGE401Challenge3/CGE401Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,87 @@
+﻿/*
+* Panos Burbulis
+* PlayerControllerX.cs
+* Challenge 3
+* Controls player character spin and particle affects and audio. Allows balloon to float
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+    private bool isLowEnough;
+    private float upperLimit = 13f;
+
+    private UIManagerX uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+        uiManager = GameObject.Find("UIManager").GetComponent<UIManagerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isLowEnough = transform.position.y < upperLimit;
+
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        }
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+            uiManager.score += 1;
+        }
+
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
+    }
+
+}
---
+++ CGE401Challenge3/CGE401Challenge3/Assets/Challenge 3/Scripts/UIManagerX.cs
@@ -0,0 +1,62 @@
+﻿/*
+* Panos Burbulis
+* UIManagerX.cs
+* Challenge 3
+* Displays score. Displays win/loss text on gameOver
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManagerX : MonoBehaviour
+{
+    public Text scoreText;
+    public int score = 0;
+    public PlayerControllerX playerX;
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+        if (playerX == null)
+        {
+            playerX = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+        }
+
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerX.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+
+        if (playerX.gameOver && !won)
+        {
+            scoreText.text = "You Lose!\nPress R to Try Again!";
+        }
+
+        if (score >= 10)
+        {
+            playerX.gameOver = true;
+            won = true;
+
+            scoreText.text = "You Win!\nPress R to Try Again!";
+        }
+
+        if (playerX.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
